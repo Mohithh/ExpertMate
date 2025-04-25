bdd9feb8dbde297bee6a0a4aa6f4bfde3ea14e26
Mohithh <mohityadavhh@gmail.com>
2025-04-25 16:28:40 +0530

    first

--- app/StartDispute/page.js
+++ app/StartDispute/page.js
@@ -49,7 +49,7 @@ const StartDispute = () => {
             <>
                 <Header />
                 <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-6">
-                    <div className="max-w-6xl mx-auto">Loading...</div>
+                    <div className="max-w-6xl mx-auto">Loading....</div>
                 </div>
                 <Footer />
             </>
